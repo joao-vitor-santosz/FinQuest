21322b482ac85fc53b3e918217bf04fcf6a46db0
João Vítor <joaovns0812@gmail.com>
2026-07-02 18:19:24 -0400

    feat: adiciona gráfico de transações

--- src/components/DashboardHome/index.tsx
+++ src/components/DashboardHome/index.tsx
@@ -1,6 +1,7 @@
 import { TrendingUp, TrendingDown, Clock } from "lucide-react";
 import { MetricCard } from "../MetricCard/index";
 import { TransactionList } from "../TransactionList/index";
+import { CategoryChart } from "../CategoryChart";
 
 export const DashboardHome = () => {
   return (
@@ -31,10 +32,8 @@ export const DashboardHome = () => {
       {/* Gráfico + Transações */}
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full items-stretch">
         
-        {/* COLUNA ESQUERDA: Espaço reservado para o Gráfico */}
-        <div className="p-6 rounded-2xl bg-bg-card/40 border border-border-glass backdrop-blur-md flex flex-col justify-center items-center min-h-75">
-          <span className="text-text-secondary text-sm">Espaço do Gráfico de Pizza</span>
-        </div>
+        {/* Gráfico */}
+        <CategoryChart />
 
         {/* COLUNA DIREITA: Lista de Transações real */}
         <TransactionList />
---
+++ src/mocks/chartData.ts
@@ -0,0 +1,7 @@
+// Dados fictícios baseados no seu mockup
+export const chartData = [
+  { name: "Alimentação", value: 40, color: "#34d399" }, // Verde (Income)
+  { name: "Transporte", value: 25, color: "#60a5fa" }, // Azul
+  { name: "Lazer", value: 20, color: "#fbbf24" },      // Amarelo/Laranja
+  { name: "Lucro", value: 15, color: "#c084fc" },      // Roxo
+];
